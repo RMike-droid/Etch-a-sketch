cd361aab575a7991ae1bb53b34feea5c75128922
RMike-droid <galyenr@gmail.com>
2024-02-20 09:31:39 -0600

    Finish the project

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const parent = document.querySelector('#container');
+const gridContainer = document.querySelector('.grid');
+const btnGenerateGrid = document.getElementById('generateGrid');
 
-for (let i = 0; i < 256; i++) {
-    const newDiv = document.createElement('div');
-    newDiv.style.cssText = "border: 1px solid black; height: 25px; width: 25px";  
-    parent.appendChild(newDiv);
+
+function changeColorTrail(e) {
+    e.target.style.backgroundColor = 'black';
+    e.target.style.color = 'black';
+}
+
+btnGenerateGrid.addEventListener('click', generateGrid);
+
+
+function generateGrid() {
+        removeAllChildren(gridContainer);
+        
+        let sizeNewGrid = window.prompt('What is the canvas size?', 16);
+
+        if(sizeNewGrid > 100) {
+            sizeNewGrid = 100;
+        }
+
+
+
+    const sizeDiv = (800 / sizeNewGrid) - 2;
+    const divNumbers = sizeNewGrid * sizeNewGrid;
+
+    for(let i = 0; i < divNumbers; i++) {
+        const gridInsertion = document.createElement('div');
+        gridInsertion.style.border = '1px solid black';
+        gridInsertion.style.width = sizeDiv+'px';
+        gridInsertion.style.width = sizeDiv+'px';
+        gridInsertion.addEventListener('mouseover', changeColorTrail);
+        gridContainer.appendChild(gridInsertion);
+    }
+}
+
+function removeAllChildren(parentElement) {
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
 }
